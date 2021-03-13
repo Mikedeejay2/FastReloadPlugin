ab9143d24209bf8a906893cfacc42b2f40ab745b
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2021-03-13 01:51:35 -0500

    Add tab completer for fast reload command

--- src/main/java/com/mikedeejay2/fastreload/commands/FastReloadCommand.java
+++ src/main/java/com/mikedeejay2/fastreload/commands/FastReloadCommand.java
@@ -1,14 +1,20 @@
 package com.mikedeejay2.fastreload.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+import org.bukkit.plugin.Plugin;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
-public class FastReloadCommand implements CommandExecutor
+public class FastReloadCommand implements TabExecutor
 {
     private final BiConsumer<CommandSender, String[]> reloader;
 
@@ -22,5 +28,15 @@ public class FastReloadCommand implements CommandExecutor
     {
         reloader.accept(sender, args);
         return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args)
+    {
+        if(args.length != 1) return null;
+        List<String> tabCompletes = Arrays.stream(Bukkit.getPluginManager().getPlugins())
+                                          .map(Plugin::getName)
+                                          .collect(Collectors.toList());
+        return tabCompletes;
     }
 }
