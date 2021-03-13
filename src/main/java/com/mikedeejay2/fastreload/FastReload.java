c5e4bf6d76fbc400309cc840f6203ba223ccdc4f
Mikedeejay2 <58639173+Mikedeejay2@users.noreply.github.com>
2021-03-12 19:20:49 -0500

    Manually disable plugin commands

--- src/main/java/com/mikedeejay2/fastreload/FastReload.java
+++ src/main/java/com/mikedeejay2/fastreload/FastReload.java
@@ -3,16 +3,21 @@ package com.mikedeejay2.fastreload;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Server;
+import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.SimpleCommandMap;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.server.ServerLoadEvent;
 import org.bukkit.permissions.Permission;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginLoadOrder;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.*;
 
 public final class FastReload extends JavaPlugin
 {
@@ -72,11 +77,63 @@ public final class FastReload extends JavaPlugin
         this.getLogger().info(String.format(ChatColor.RED + "Player %s reloaded the server's plugins!", sender.getName()));
         PluginManager pluginManager = Bukkit.getPluginManager();
         Server server = this.getServer();
+        for(Plugin plugin : pluginManager.getPlugins())
+        {
+            String pluginName = plugin.getName();
+            pluginManager.disablePlugin(pluginManager.getPlugin(pluginName));
+            pluginManager.enablePlugin(pluginManager.getPlugin(pluginName));
+        }
 
         long startTime = System.currentTimeMillis();
-        pluginManager.clearPlugins();
+
+        // Manual removal of plugin commands start
+        SimpleCommandMap commands = null;
+        Map<String, Command> knownCommands = null;
         try
         {
+            Method cmdMapMethod = server.getClass().getDeclaredMethod("getCommandMap");
+            commands = (SimpleCommandMap) cmdMapMethod.invoke(server);
+            Field knownCmdsField = SimpleCommandMap.class.getDeclaredField("knownCommands");
+            knownCmdsField.setAccessible(true);
+            knownCommands = (Map<String, Command>) knownCmdsField.get(commands);
+        }
+        catch(NoSuchMethodException | IllegalAccessException | InvocationTargetException | NoSuchFieldException e)
+        {
+            e.printStackTrace();
+            return;
+        }
+
+        for(Plugin plugin : pluginManager.getPlugins())
+        {
+            String fallbackPrefix = plugin.getDescription().getName().toLowerCase(java.util.Locale.ENGLISH).trim();
+            for(String name : plugin.getDescription().getCommands().keySet())
+            {
+                Command mainCmd = commands.getCommand(name);
+                if(mainCmd == null) continue;
+                List<String> aliases = mainCmd.getAliases();
+
+                mainCmd.unregister(commands);
+                knownCommands.remove(name);
+                knownCommands.remove(fallbackPrefix + ":" + name);
+
+                for(String alias : aliases)
+                {
+                    Command aliasCmd = commands.getCommand(alias);
+                    if(aliasCmd == null) continue;
+                    aliasCmd.unregister(commands);
+                    knownCommands.remove(alias);
+                    knownCommands.remove(fallbackPrefix + ":" + alias);
+                }
+            }
+        }
+        // Manual removal of plugin commands end
+
+        // Manual reload of plugins start
+        pluginManager.clearPlugins();
+
+        try
+        {
+
             Method reloadMethod = server.getClass().getDeclaredMethod("loadPlugins");
             Method enableMethod = server.getClass().getDeclaredMethod("enablePlugins", PluginLoadOrder.class);
             reloadMethod.invoke(server);
@@ -86,8 +143,13 @@ public final class FastReload extends JavaPlugin
         catch(NoSuchMethodException | IllegalAccessException | InvocationTargetException e)
         {
             e.printStackTrace();
+            return;
         }
+        // Manual reload of plugins end
+
+        // Call the server load event. Probably not required but there just in case a plugin uses for something.
         this.getServer().getPluginManager().callEvent(new ServerLoadEvent(ServerLoadEvent.LoadType.RELOAD));
+
         long endTime = System.currentTimeMillis();
         long differenceTime = endTime - startTime;
         sender.sendMessage(ChatColor.GREEN + "The server has successfully reloaded all plugins in " + differenceTime + "ms.");
